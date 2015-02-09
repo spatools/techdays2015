88d78b16f416cca26a1e49f377343f3440814df1
SomaticIT <contact@somatic.fr>
2015-02-09 17:25:00 +0100

    Add a versioning task

---
+++ demo/tasks/versioning.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+
+    grunt.registerMultiTask("versioning", function () {
+        var path = require("path"),
+            pkg = grunt.file.readJSON("package.json");
+
+        this.files.forEach(function (file) {
+            file.src.forEach(function (src) {
+                var content = grunt.file.read(src),
+                    destPath = path.join(file.dest, path.basename(src));
+
+                if (content.indexOf("{{version}}") !== -1) {
+                    content = content.replace(/\{\{version\}\}/g, pkg.version);
+
+                    grunt.file.write(destPath, content);
+
+                    grunt.log.ok("Version added to " + destPath);
+                }
+            });
+        });
+    });
+};
