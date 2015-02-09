2657d9f0444de669de7ede5af35408dfba97c1f5
SomaticIT <contact@somatic.fr>
2015-02-09 16:57:14 +0100

    Update user router and repository view

--- demo/app/scripts/routes/user.js
+++ demo/app/scripts/routes/user.js
@@ -4,19 +4,30 @@ define([
     'jquery',
     'backbone',
     'models/user',
-    'views/user'
-], function ($, Backbone, User, UserView) {
+    'models/repository',
+    'views/user',
+    'views/repository'
+], function ($, Backbone, User, Repository, UserView, RepositoryView) {
     'use strict';
 
     var UserRouter = Backbone.Router.extend({
         routes: {
             "users/:login": "user",
+            "users/:login/:repo": "user_repo"
         },
 
         user: function (login) {
             var user = new User({ login: login });
             user.fetch().then(function () {
                 var view = new UserView({ el: $("#main"), model: user });
+                view.render();
+            });
+        },
+
+        user_repo: function (login, repo) {
+            var repo = new Repository({ full_name: login + "/" + repo });
+            repo.fetch().then(function () {
+                var view = new RepositoryView({ el: $("#main"), model: repo });
                 view.render();
             });
         }
