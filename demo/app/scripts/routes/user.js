06ca9b489f9bd0d1eb8eba877b870b82b51f54c7
SomaticIT <contact@somatic.fr>
2015-02-09 16:05:25 +0100

    Run yo backbone:router user

---
+++ demo/app/scripts/routes/user.js
@@ -0,0 +1,16 @@
+/*global define*/
+
+define([
+    'jquery',
+    'backbone'
+], function ($, Backbone) {
+    'use strict';
+
+    var UserRouter = Backbone.Router.extend({
+        routes: {
+        }
+
+    });
+
+    return UserRouter;
+});
