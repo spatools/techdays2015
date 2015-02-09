db56fa47ea2b89e54f700c1a421de0ae2c438a50
SomaticIT <contact@somatic.fr>
2015-02-09 16:47:11 +0100

    Update user model to add repos collection

--- demo/app/scripts/models/user.js
+++ demo/app/scripts/models/user.js
@@ -2,14 +2,18 @@
 
 define([
     'underscore',
-    'backbone'
-], function (_, Backbone) {
+    'backbone',
+    'collections/userrepos'
+], function (_, Backbone, UserReposCollection) {
     'use strict';
 
     var UserModel = Backbone.Model.extend({
 
         initialize: function() {
             this.url = "https://api.github.com/users/" + this.get("login");
+
+            var repos = new UserReposCollection({ login: this.get("login") });
+            this.set("repos", repos);
         },
 
         defaults: {
@@ -20,6 +24,23 @@ define([
 
         parse: function(response, options)  {
             return response;
+        },
+
+        fetch: function () {
+            var self = this,
+                args = arguments;
+
+            return this.get("repos").fetch().then(function () {
+                return Backbone.Model.prototype.fetch.apply(self, args);
+            });
+        },
+        toJSON: function () {
+            return _.extend(
+                Backbone.Model.prototype.toJSON.apply(this, arguments),
+                {
+                    repos: this.get("repos").toJSON()
+                }
+            );
         }
     });
 
