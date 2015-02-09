1671486bd91c31fb40118bc6a2d464d1678a0074
SomaticIT <contact@somatic.fr>
2015-02-09 16:43:51 +0100

    Set url in user repos collection

--- demo/app/scripts/collections/userrepos.js
+++ demo/app/scripts/collections/userrepos.js
@@ -8,7 +8,16 @@ define([
     'use strict';
 
     var UserReposCollection = Backbone.Collection.extend({
-        model: RepositoryModel
+        model: RepositoryModel,
+
+        initialize: function (models, options) {
+            if (!options && !_.isArray(models)) {
+                options = models;
+                models = null;
+            }
+
+            this.url = "https://api.github.com/users/" + options.login + "/repos";
+        }
     });
 
     return UserReposCollection;
